6fa8194f17ddc8e40936e789825a8f5428f27cb9
leehj050211 <leehj050211@gmail.com>
2022-06-07 15:58:34 +0900

    Add: 채팅 아이템 컴포넌트, 다크 모드 css

---
+++ src/components/chat-item.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface propsType {
+    guestId: number,
+    date: Date,
+    content: string
+}
+
+const ChatItem: React.FC<propsType> = (props: propsType) => {
+    return (
+        <div className='chat--item'>
+            <div className='chat--item--profile'>
+                <span>{props.guestId}</span>
+            </div>
+            <div className='chat--item--content-wrap'>
+                <div className='chat--item--header'>
+                    <div className='chat--item--nickname'>{`${props.guestId}번째 방문자`}</div>
+                    <div className='chat--item--date'>{props.date.toLocaleString()}</div>
+                </div>
+                <div className='chat--item--content'>{props.content}</div>
+            </div>
+        </div>
+    );
+}
+export default ChatItem;
